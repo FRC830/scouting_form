ddc1355eda7be9c28992a8b59cacafba711de906
cbott <cbott6@gmail.com>
2016-02-13 16:15:05 -0500

    modify save file to include computer name, might be helpful

--- server.py
+++ server.py
@@ -18,8 +18,9 @@ def test():
 @app.route('/form', methods=('GET', 'POST'))
 def form():
     f = form_helper.load_form(os.path.join(os.getcwd(), '..', 'web', 'fields.py'))()
-    if f.validate_on_submit():
-        print(f.data)
-        exporter.save_data(f.data, os.path.join(os.getcwd(), '..', 'test.csv'))
+    if f.validate_on_submit():#Form data is valid
+    	#this will save data entered on the form to a new line in the csv file
+    	#Ex: Red1_scouting_data.csv (if running on computer Red1)
+        exporter.save_data(f.data, os.path.join(os.getcwd(), '..', os.eviron['COMPUTERNAME']+'_scouting_data.csv'))
         return flask.redirect('/form')
     return flask.render_template('form.html', form=f)
