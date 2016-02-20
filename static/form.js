1bbb6cb949f4782179c31be96aee102932705cb9
cbott <cbott6@gmail.com>
2016-02-20 16:05:34 -0500

    javascript to add +- buttons to the input fields

--- static/form.js
+++ static/form.js
@@ -5,12 +5,31 @@ $(function() {
     $('#scouting-form').on('submit', function() {
         localStorage.setItem('last_match_id',$('#match_id').val());
     });
-    //Allow input boxes to be changed with arrow keys
+    //Allows input boxes to be changed with arrow keys
     $("input[type='text']").keydown(function(e){
     	if(e.keyCode == 38){
     		this.value = parseInt(this.value)+1;
     	} else if (e.keyCode == 40){
     		this.value = parseInt(this.value)-1;
     	}
+    });
+
+    //Add +/- buttons next to input fields so it is easy on touchscreen
+    function mkbutton() {
+        return $('<a>').attr('href', '#').addClass('btn btn-default').click(function(e){e.preventDefault();});
+    }
+    $('input[type=text]').each(function(i, e) {
+        var increment = function(n) {
+            return function() {
+                $(e).val(Number($(e).val()) + n);
+                $(e).trigger('keyup');
+            }
+        }
+        var button_inc = mkbutton().text('+').insertAfter($(e)).click(increment(1));
+        var button_dec = mkbutton().text('-').insertBefore($(e)).click(increment(-1));
+        var width = 8;
+        var height = 15;
+        button_inc.width(width).height(height);
+        button_dec.width(width).height(height);
     });
 });
