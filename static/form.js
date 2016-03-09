b600e333465980b79f08c05a04600efbee5eb283
830-A <A@830.local>
2016-03-09 18:41:31 -0500

    Stop match ID from incrementing on validation failure

--- static/form.js
+++ static/form.js
@@ -12,7 +12,8 @@ function generateValidationRules(form) {
 $(function() {
 	//This allows the Match ID field to be automatically filled
 	//based on the previous value entered + 1
-    $('#match_id').val(Number(localStorage.getItem('last_match_id')) + 1);
+    if (!$('#match_id').val())
+        $('#match_id').val(Number(localStorage.getItem('last_match_id')) + 1);
     $('#scouting-form').on('submit', function() {
         localStorage.setItem('last_match_id',$('#match_id').val());
     }).validate({
